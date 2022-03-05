c0a23eae8cb5c3dd04a59e61d5d0932b8737a1af
JUNE <jnbm@ijunbeom-ui-MacBookPro.local>
2022-03-05 15:09:31 +0900

    6.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app'
 import Layout from './_layout'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout>
+  return <>
+  <Head>
+    <title>Nike Store</title>
+  </Head>
+  <Layout>
       <Component {...pageProps} />
     </Layout>
+  </>
 }
 
 export default MyApp
